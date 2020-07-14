468e139064594879ce481406707b03a8b0e5f1e8
julio <juliosantos.26es@gmail.com>
2020-07-14 12:10:41 -0400

    se modifico el span

--- src/views/Client/Carrito.vue
+++ src/views/Client/Carrito.vue
@@ -9,7 +9,7 @@
 							Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
 							in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
 						</p>
-						<span>Un</span>
+						<span>Un cualquier cosa</span>
 						<b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
 					</div>
 				</b-sidebar>
